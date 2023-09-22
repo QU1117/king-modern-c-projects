696fffb9162d0537be429f1863e5192dbbf3d7c8
QU1117 <volgirre@gmail.com>
2023-09-22 20:18:43 +0300

    remove useless variable in chapter 4 project 1

--- chapter-4/projects/project-1/two-digits-reversed.c
+++ chapter-4/projects/project-1/two-digits-reversed.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-    int user_number, reversal_number, first_number, last_number;
+    int user_number, first_number, last_number;
 
     printf("Enter a two digit number: ");
     scanf("%d", &user_number);
